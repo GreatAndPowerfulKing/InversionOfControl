84603655fe2a7fafd5e6de06add47292efcc1d1a
iKing <pedos.vm@icloud.com>
2016-04-14 11:12:12 +0300

    All tasks performed

--- interfaceWrapper/en/application.js
+++ interfaceWrapper/en/application.js
@@ -1,6 +1,20 @@
 
+fs.appendFile("output.txt", "Some text", function(args) {
+	console.log("From fs append");
+});
+
 var fileName = './README.md';
 console.log('Application going to read ' + fileName);
-fs.readFile(fileName, function(err, src) {
-	console.log('File ' + fileName + ' size ' + src.length);
-});
+var interval = setInterval(function () {
+		fs.readFile(fileName, function(err, src) {
+		console.log('File ' + fileName + ' size ' + src.length);
+	});
+}, 1000)
+
+var timeout = function () {
+	setTimeout(function () {
+		clearInterval(interval);
+	}, 10000);
+};
+
+timeout();
--- interfaceWrapper/en/framework.js
+++ interfaceWrapper/en/framework.js
@@ -3,27 +3,61 @@
 var fs = require('fs'),
     vm = require('vm');
 
+var calls = 0;
+
 function cloneInterface(anInterface) {
   var clone = {};
   for (var key in anInterface) {
-    clone[key] = anInterface[key];
+    clone[key] = wrapFunction(key, anInterface[key]);
   }
   return clone;
 }
+
+function wrapFunction(fnName, fn) {
+  return function wrapper() {
+    calls++;
+    var args = [];
+    Array.prototype.push.apply(args, arguments);
+    var callback = args[args.length - 1];
+    if (typeof(callback) ===  "function") {
+      args[args.length - 1] = function() {
+        var callbackArgs = arguments;
+        console.log(callbackArgs);
+        callback.apply(undefined, callbackArgs);
+      }
+    }
+    console.log('Call: ' + fnName);
+    console.dir(args);
+    return fn.apply(undefined, args);
+  }
+}
+
+var interval = setInterval(function() {
+  console.log("\nCalls count: " + calls + "\n");
+}, 3000)
+
+var timeout = function () {
+  setTimeout(function () {
+    clearInterval(interval);
+  }, 15000);
+};
+
+timeout();
 
 // Create a hash for application sandbox
 var context = {
   module: {},
   console: console,
   // Forward link to fs API into sandbox
-  fs: cloneInterface(fs)
+  fs: cloneInterface(fs),
+  setTimeout: setTimeout,
+  setInterval: setInterval,
+  clearInterval: clearInterval
 };
 
 // Turn hash into context
 context.global = context;
 var sandbox = vm.createContext(context);
-
-
 
 // Read an application source code from the file
 var fileName = './application.js';
